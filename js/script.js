d5ed1c3039c188d44e027148aab5cb3a95c5c016
Sivatharan <sivatharanoff97@gmail.com>
2025-11-16 07:52:30 +0530

    implement smooth scolling to the page

--- js/script.js
+++ js/script.js
@@ -9,6 +9,33 @@ const btnNav = document.querySelector(".btn-mobile-nav");
 btnNav.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+//Smooth Scrolling animation
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href"); //this will assign value of id as href.. eg: #cta
+
+    //scroll backto top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    //scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+    //close the mobile nav
+    link.classList.contains("main-nav-link") &&
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
